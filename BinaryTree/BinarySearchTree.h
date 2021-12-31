d0073e37fddaf2130484e61deb8ce8a89ff1ed9d
Dogkan Sarac <dogkansarac@sabanciuniv.edu>
2021-12-31 17:31:51 +0300

    Add project files.

---
+++ BinaryTree/BinarySearchTree.h
@@ -0,0 +1,14 @@
+#pragma once
+class BinarySearchTree
+{
+	public:
+		BinarySearchTree();
+		~BinarySearchTree();
+
+		void insert(int n);
+		void find(int n);
+		void remove(int n);
+	private:
+
+};
+
